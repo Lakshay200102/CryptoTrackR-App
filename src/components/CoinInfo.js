a6961362b210cc4c3c0be3c32b97eb935a152e77
Lakshay Kumar <2001Lakshaygdkumar02@gmail.com>
2022-11-01 19:12:12 +0530

    Charting Graph Implemented

--- src/components/CoinInfo.js
+++ src/components/CoinInfo.js
@@ -1,9 +1,150 @@
-import React from 'react'
+import {
+  CircularProgress,
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from "chart.js";
+import { HistoricalChart } from "../config/api";
+import { chartDays } from "../config/data";
+import { CryptoState } from "../CryptoContext";
+import SelectButton from "./SelectButton";
 
-const CoinInfo = () => {
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+);
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    width: "75%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 25,
+    padding: 40,
+    [theme.breakpoints.down("md")]: {
+      width: "100%",
+      marginTop: 0,
+      padding: 20,
+      paddingTop: 0,
+    },
+  },
+}));
+
+const CoinInfo = ({ coin }) => {
+  const [historicData, setHistoricData] = useState();
+  const [days, setDays] = useState(1);
+  const { currency } = CryptoState();
+
+  const classes = useStyles();
+
+  const fetchHistoricData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+
+    setHistoricData(data.prices);
+  };
+
+  console.log(coin);
+
+  useEffect(() => {
+    fetchHistoricData();
+  }, [currency, days]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
   return (
-    <div>CoinInfo</div>
-  )
-}
+    <ThemeProvider theme={darkTheme}>
+      <div className={classes.container}>
+        {!historicData ? (
+          <CircularProgress
+            style={{ color: "gold" }}
+            size={250}
+            thickness={1}
+          />
+        ) : (
+          <>
+            <Line
+              data={{
+                labels: historicData.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let time =
+                    date.getHours() > 12
+                      ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                      : `${date.getHours()}:${date.getMinutes()} AM`;
 
-export default CoinInfo
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
+                datasets: [
+                  {
+                    label: `Price (Past ${days} Days) in ${currency}`,
+                    data: historicData.map((coin) => coin[1]),
+                    backgroundColor: "rgba(216, 187, 93, 0.1)",
+                    borderColor: "#EEBC1D",
+                    tension: 0.1,
+                    fill: true,
+                  },
+                ],
+              }}
+              options={{
+                elements: {
+                  point: {
+                    radius: 1,
+                  },
+                },
+              }}
+            />
+            <div>{console.log("Hello")}</div>
+            <div
+              style={{
+                display: "flex",
+                marginTop: 20,
+                justifyContent: "space-around",
+                width: "100%",
+              }}
+            >
+              {chartDays.map((day) => (
+                <SelectButton
+                  key={day.value}
+                  onClick={() => setDays(day.value)}
+                  selected={day.value === days}
+                >
+                  {day.label}
+                </SelectButton>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default CoinInfo;
